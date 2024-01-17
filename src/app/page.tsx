ef525c1ffda2d700a7c1f40948b8af7d47ecb341
Hramchenko Alexey <154812015+AlexeyQwefix@users.noreply.github.com>
2024-01-17 19:25:05 +0300

    show todays pic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,21 @@ import { useState } from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 import Button from "@mui/material/Button";
 import fetchNasaImage from "@/requester/requester";
+import { NasaPicResp } from "@/types/nasa-pic-resp";
+import ViewNasaPic from "./ViewNasaPic";
 
 export default function Home() {
   const [isPicLoading, setIsPicLoading] = useState(false);
+  const [picture, setPicture] = useState<NasaPicResp | null>(null);
 
   const getPictureOFTheDay = async () => {
-    console.warn("hello");
-    fetchNasaImage()
+    setIsPicLoading(true);
+    try {
+      setPicture(await fetchNasaImage());
+    } catch (e) {
+    } finally {
+      setIsPicLoading(false);
+    }
   };
 
   return (
@@ -20,15 +28,22 @@ export default function Home() {
         <h1 className="bg-white px-3 py-6 text-xl font-bold text-center rounded-md">
           UI Astronomy Picture of the day
         </h1>
-        <DatePicker label="Basic date picker" />
-        <Button
-          variant="outlined"
-          className="self-center"
-          onClick={getPictureOFTheDay}
-        >
-          Get Pic
-        </Button>
-        <ClipLoader></ClipLoader>
+        <div className="bg-white px-3 py-6 flex flex-wrap gap-1 items-center rounded-md">
+          <Button
+            disabled={isPicLoading}
+            variant="outlined"
+            className="self-center"
+            onClick={getPictureOFTheDay}
+          >
+            Get Pic
+          </Button>
+          {isPicLoading && <ClipLoader></ClipLoader>}
+        </div>
+        {!!picture && (
+          <div className="bg-white px-3 py-6 flex flex-col gap-1 items-center rounded-md">
+            <ViewNasaPic pic={picture}></ViewNasaPic>
+          </div>
+        )}
       </div>
     </main>
   );
--- src/requester/requester.ts
+++ src/requester/requester.ts
@@ -1,10 +1,12 @@
-const fetchNasaImage = async (
-  params?: {
-    date?: Date;
-    startDate?: Date;
-    endDate?: Date;
-  }
-) => {
+import { NasaPicResp } from "@/types/nasa-pic-resp";
+
+type ParamsType = {
+  date?: Date;
+  startDate?: Date;
+  endDate?: Date;
+};
+
+const fetchNasaImage = async (params?: ParamsType): Promise<NasaPicResp> => {
   const res = await fetch(
     `https://api.nasa.gov/planetary/apod?api_key=L7TbFwD6owavSy9S36qxGVBOYoD7d0PVUYoyWJit`
   );
